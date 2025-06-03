208b9ae9319806921f1eb99f254b5d7a7ceb3242
Nekomaru-PKU <Nekomaru@pku.edu.cn>
2025-06-03 08:36:52 -0400

    Remove unused files for project suspension

--- dawn-examples/src/bin/main.rs
+++
@@ -1,7 +0,0 @@
-use dawn_rs::*;
-
-fn main() {
-    let instance = Instance::new(None).unwrap();
-    println!("{:?}", Instance::get_capabilities());
-    println!("{:?}", instance.get_wgsl_language_features().as_deref());
-}
